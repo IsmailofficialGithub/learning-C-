f67c87c148257b7b40331cde2096ff9308619778
Ismail abbasi <ismail.official295@gmail.com>
2024-07-10 20:04:20 -0600

    creating employee info class and object

--- oops.cpp
+++ oops.cpp
@@ -1,48 +1,88 @@
 #include <iostream>
 using namespace std;
 
+     /// ===========class of book info 
+// class bookInfo{
+//      public:
+//      string name;
+//      string author;
+//      string bookId;
+//      bool avilable;
 
-class bookInfo{
-     public:
-     string name;
-     string author;
-     string bookId;
-     bool avilable;
+
+//      // constructor
+
+//      bookInfo(string bookName,string bookAuthor,string bookUniId ){
+//           name=bookName;
+//           author=bookAuthor;
+//           bookId=bookUniId;
+//           avilable=0;
+//      }
+
+//      // display function
+
+//     void displayBookInfo(){
+//           cout<<"Name : "<<name<<endl;
+//           cout<<"Author : "<<author<<endl;
+//           cout<<"ISBN licence : "<<bookId<<endl;
+//           cout<<"Avilable : "<<(avilable ? "Yes" :"No")<<endl;
+//      };
+
+//      // is book avalible
+//           void isAbilable(){
+//                if(avilable){
+//                     cout<<"Book is Avalible ";
+//                }else{
+//                     cout<<"Book is Not Avalible "; 
+//                }
+//           }
+// };
 
 
-     // constructor
+          // ===========class of employee management
+     class employeeInfo{
+          public: 
+          string name;
+          int empId;
+          string position;
+          float salary;
 
-     bookInfo(string bookName,string bookAuthor,string bookUniId ){
-          name=bookName;
-          author=bookAuthor;
-          bookId=bookUniId;
-          avilable=0;
-     }
+          // constructor
 
-     // display function
+          employeeInfo(string employeeName,int employeeId,string empolyeePosition,float employeeSalary ){
+               name=employeeName;
+               empId=employeeId;
+               position=empolyeePosition;
+               salary=employeeSalary;
+          }
 
-    void displayBookInfo(){
-          cout<<"Name : "<<name<<endl;
-          cout<<"Author : "<<author<<endl;
-          cout<<"ISBN licence : "<<bookId<<endl;
-          cout<<"Avilable : "<<(avilable ? "Yes" :"No")<<endl;
+          // display function or method
+
+          void displayempolyeeInfo(){
+               cout<<"Employee Name is "<<name<<endl;
+               cout<<"Employee Id is "<<empId<<endl;
+               cout<<"Employee position is "<<position<<endl;
+               cout<<"Employee current salary is "<<salary<<endl;
+          }
+
+          void increaseSalary(float amount){
+                float newSalary=salary+=amount;
+               salary=newSalary;
+                cout<<"Your salary is increase "<<amount<<" .New salary is "<<newSalary<<endl;
+          }
+
+
      };
 
-     // is book avalible
-          void isAbilable(){
-               if(avilable){
-                    cout<<"Book is Avalible ";
-               }else{
-                    cout<<"Book is Not Avalible "; 
-               }
-          }
-};
-
-
      int main(){
-          bookInfo book1("The Great Gatsby","Ismail Abbasi","9780743273565");
-          book1.displayBookInfo();
-          book1.isAbilable();
+          // bookInfo book1("The Great Gatsby","Ismail Abbasi","9780743273565");
+          // book1.displayBookInfo();
+          // book1.isAbilable();
+
+          employeeInfo emp1("ismail abbasi",540,"softwareEngenier",34.344);
+          employeeInfo emp2("ALi",541,"webDeveloper",54.344);
+          emp1,emp2.displayempolyeeInfo();
+          emp1.increaseSalary(100);
 
 
 return 0;
